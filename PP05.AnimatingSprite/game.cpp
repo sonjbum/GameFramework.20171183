de69628991e6c8e4c7642ebc81587444151f40e7
학과 게임 <hoseogame@hoseogame.onmicrosoft.com>
2018-09-21 11:56:25 +0900

    09월21일 pp05.AnimatingSprite

--- PP05.AnimatingSprite/game.cpp
+++ PP05.AnimatingSprite/game.cpp
@@ -13,11 +13,18 @@ bool Game::init(const char*title, int xpos, int ypos, int width, int height, boo
 		}
 		m_bRunning = true;
 
-		SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+		SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
 		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
 		SDL_FreeSurface(pTempSurface);
 
-		SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+
+		m_sourceRectangle.w = 128;
+		m_sourceRectangle.h = 82;
+
+		m_destinationRectangle.x = m_sourceRectangle.x = 0;
+		m_destinationRectangle.y = m_sourceRectangle.y = 0;
+		m_destinationRectangle.w = m_sourceRectangle.w;
+		m_destinationRectangle.h = m_sourceRectangle.h;
 	}
 	else {
 		return false;
@@ -27,10 +34,6 @@ bool Game::init(const char*title, int xpos, int ypos, int width, int height, boo
 
 void Game::render()
 {
-	m_destinationRectangle.x = m_sourceRectangle.x = 0;
-	m_destinationRectangle.y = m_sourceRectangle.y = 0;
-	m_destinationRectangle.w =m_sourceRectangle.w;
-	m_destinationRectangle.h = m_sourceRectangle.h;
 	SDL_RenderClear(m_pRenderer);
 	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
 	SDL_RenderPresent(m_pRenderer);
@@ -46,7 +49,7 @@ void Game::clean()
 
 void Game::update()
 {
-
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
 }
 
 void Game::handleEvents()
